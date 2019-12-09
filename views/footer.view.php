f2806aba313285a8ac0dce56dcd0b6be002363b1
abdarrahman abouzaid <abdarrahman.abouzaid@gmail.com>
2019-12-10 01:00:30 +0200

    dry up views

---
+++ views/footer.view.php
@@ -0,0 +1,6 @@
+    <!-- JS -->
+    <script src="/public/js/jquery.min.js"></script>
+    <script src="/public/js/main.js"></script>
+    
+</body><!-- This templates was made by Colorlib (https://colorlib.com) -->
+</html>
