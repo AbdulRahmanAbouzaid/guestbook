b7866013fe0efdbb85841d437700e4a8fed7c5c5
abdarrahman abouzaid <abdarrahman.abouzaid@gmail.com>
2019-12-09 21:21:16 +0200

    auth/user login

--- controllers/UsersController.php
+++ controllers/UsersController.php
@@ -26,4 +26,36 @@ class UsersController extends Controller {
 
         return $this->redirectTo('/');
     }
+
+
+
+    public function showLoginForm()
+    {
+        require 'views/login.view.php';
+    }
+
+
+
+    public function login()
+    {
+        session_start();        
+
+        $this->validate($_POST, [
+            'email' => 'required',
+            'password' => 'required'
+        ]);
+
+        $email = $_POST['email'];
+        if($result = User::where('email', '=', $email)){
+            $user = $result[0];
+            $input_password = $_POST['password'];
+            if(password_verify($input_password, $user->password)){
+                $_SESSION['user_id'] = $user->id;
+                return $this->redirectTo('/');
+            }
+        }
+        $_SESSION['errors'][] = 'Invalid Email or Password, please try agian';
+
+        return $this->redirectTo('/login');
+    }
 }
--- routes.php
+++ routes.php
@@ -3,3 +3,6 @@
 $router->get('', 'PagesController@index');
 $router->get('register', 'UsersController@showRegisterForm');
 $router->post('register', 'UsersController@register');
+
+$router->get('login', 'UsersController@showLoginForm');
+$router->post('login', 'UsersController@login');
